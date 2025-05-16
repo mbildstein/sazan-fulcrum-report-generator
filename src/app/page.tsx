da31bebd9a2d47ac7006c1f0addfe1959ef09350
Michael Bildstein <michael.bildstein@gmail.com>
2025-05-16 15:29:51 -0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Document, Packer, Paragraph, TextRun } from "docx";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-xl">Sazan Group</h1>
+      <h2 className="text-lg">Fulcrum FCA Report Generator</h2>
+
+      <button></button>
+    </div>
+  );
+}
